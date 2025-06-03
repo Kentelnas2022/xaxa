06d1a73fa0242e67e3dbfe67f474c802b81c744a
Kent Zorel B. Elnas <141716943+Kentelnas2022@users.noreply.github.com>
2025-06-03 22:38:45 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,28 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SplashScreen from './components/SplashScreen';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
+import HistoryLog from './components/HistoryLog';
 import Loading from './components/Loading';
 import Profile from './components/Profile';
 import Emergency from './components/Emergency';
 import ProtectedRoute from './components/ProtectedRoute';
 import LayoutNav from './components/LayoutNav';
-import Location from './components/Location'; 
+import Location from './components/Location';
 import Settings from './components/Settings';
-import SplashScreen from './components/SplashScreen';
-
 import './App.css';
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<SplashScreen />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/layoutnav" element={<LayoutNav />} />
         <Route path="/loading" element={<Loading />} />
-
-        <Route
-          path="/splashscreen"
-          element={
-            <ProtectedRoute>
-              <SplashScreen />
-            </ProtectedRoute>
-          }
-        />
-
+        <Route path="/historylog" element={<HistoryLog />} />
 
         <Route
           path="/dashboard"
@@ -42,7 +32,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-      
         <Route
           path="/location"
           element={
@@ -51,7 +40,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-
         <Route
           path="/profile"
           element={
